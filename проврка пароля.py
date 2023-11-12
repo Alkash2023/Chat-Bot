88525e51accb8b641933e30680c94b0272b1f0ce
Alkash2023 <147981923+Alkash2023@users.noreply.github.com>
2023-11-12 19:02:07 +0700

    Create проврка пароля.py

---
+++ проврка пароля.py
@@ -0,0 +1,9 @@
+password=input("введите пароль ")
+def pass_chek(password):
+    if password.isalpha() or password.isdigit() or password.islower() or password.isupper() or len(password)<8:
+        print("Пароль ненадежный")
+    else:
+        if '!' in password or  '=' in password or '@' in password or  '#' in password or  '№' in password:
+            print("пароль надежный")
+    else:print("пароль ненадежный")
+pass_chek(password) 

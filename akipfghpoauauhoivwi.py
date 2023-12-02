81fd8427d4aea97a5498705783fd5154c3e41f1e
Alkash2023 <147981923+Alkash2023@users.noreply.github.com>
2023-12-02 20:34:06 +0700

    Create akipfghpoauauhoivwi.py
    d tuacfyiJOPK -9iuphif qeijeije'jgq wii;ihjv shvs;hvo;havo;
    svsvshvs
    svjsivsinhvishvosihvskekihisknenk 
    fpkfpksd nssfksfjsipfhsip
    fffffffffffffffff
    f

---
+++ akipfghpoauauhoivwi.py
@@ -0,0 +1,12 @@
+z=int(input("введите число "))
+j=int(input("введите число "))
+def i(z,j):
+    if (10<z and 999>z) and (10<j and 999>j):
+        if j==z:
+            print("совпадение цифр")
+        else:
+            print("числа несовпали")
+    else:
+        print("неправильно")
+i(z,j)
+

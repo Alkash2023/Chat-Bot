aeedaaa5b76cb91830adf1dacaa8399403ec3e48
Alkash2023 <147981923+Alkash2023@users.noreply.github.com>
2023-12-02 19:59:40 +0700

    Create 2 или 3 знака.py
    q3weoisrduyr4 erbrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrehssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

---
+++ 2 или 3 знака.py
@@ -0,0 +1,9 @@
+z=int(input("введите число "))
+def pass_chek(z):
+    if 10<z and 99>z:
+        print("2 значное")
+    elif 100<z and 999>z:
+        print("3 значное")
+    else:
+        ("пароль неверный")
+pass_chek(z) 

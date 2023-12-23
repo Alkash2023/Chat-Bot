3dfebd8b82477e99f580237fc490c1f966e7d018
Alkash2023 <147981923+Alkash2023@users.noreply.github.com>
2023-12-23 19:18:56 +0700

    Create razymen li i.py
    sorry

---
+++ razymen li i.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+time.sleep(1)
+pyautogui.moveTo(150,760)
+pyautogui.click(button = "left")
+time.sleep(1)
+pyautogui.moveTo(1150,10)
+pyautogui.click(button = "left")
+time.sleep(1)
+pyautogui.moveTo(700,440)
+pyautogui.click(button = "left")
+time.sleep(1)
+pyautogui.write('hfpevty kb z?', interval=0.50)
+pyautogui.press('enter')

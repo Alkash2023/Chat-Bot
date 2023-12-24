2211524b92a052f64162a26a677e7b7252b7dd99
Alkash2023 <147981923+Alkash2023@users.noreply.github.com>
2023-12-24 16:58:15 +0700

    Create poisk.py

---
+++ poisk.py
@@ -0,0 +1,15 @@
+i=(input("Введите что нужно найти "))
+import pyautogui
+import time
+time.sleep(1)
+pyautogui.moveTo(150,760)
+pyautogui.click(button = "left")
+time.sleep(1)
+pyautogui.moveTo(1150,10)
+pyautogui.click(button = "left")
+time.sleep(1)
+pyautogui.moveTo(700,440)
+pyautogui.click(button = "left")
+time.sleep(1)
+pyautogui.write(i, interval=0.50)
+pyautogui.press('enter')
